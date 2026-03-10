45ba035b580cb77808cb81b30964a9e671fd2d38
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-03-10 21:14:45 +0000

    AuraStream API Auth (notifications): add email_tasks with an email sender, deploy celery for task managing, create templates for emails

---
+++ src/core/celery.py
@@ -0,0 +1,20 @@
+from celery import Celery
+
+from core.settings import settings
+
+REDIS_URL = settings.REDIS_URL
+
+celery_app = Celery(
+    "aura_stream_tasks",
+    broker=REDIS_URL,
+    backend=REDIS_URL,
+    include=["tasks.email_tasks"],
+)
+
+celery_app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
+    enable_utc=True,
+)
