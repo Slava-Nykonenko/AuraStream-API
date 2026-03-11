dd575683e8369d0e9b19e3abeebaf36e28fc7311
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2026-03-11 20:30:45 +0000

    AuraStream API. Auth: implement cleanup tasks for deleting expired tokens from the database with celery-beat scheduling for it

--- src/core/celery.py
+++ src/core/celery.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from core.settings import settings
 
@@ -8,7 +9,7 @@ celery_app = Celery(
     "aura_stream_tasks",
     broker=REDIS_URL,
     backend=REDIS_URL,
-    include=["tasks.email_tasks"],
+    include=["tasks.email_tasks", "tasks.cleanup_tasks"],
 )
 
 celery_app.conf.update(
@@ -18,3 +19,10 @@ celery_app.conf.update(
     timezone="UTC",
     enable_utc=True,
 )
+
+celery_app.conf.beat_schedule = {
+    "cleanup-expired-tokens-every-hour": {
+        "task": "tasks.cleanup_tasks.cleanup_expired_tokens",
+        "schedule": crontab(minute=0, hour="*/1"),
+    },
+}
